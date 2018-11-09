71332eb5065bfce8280d24dcc94494a88597260f
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2018-11-09 13:44:39 -0700

    additional work done

---
+++ assets/javascript/test.js
@@ -0,0 +1,156 @@
+// console.log(guessTheScotch);
+
+// console.log(scotch);
+
+
+// console.log(keyPressed)
+
+// function: resetGame(){
+//     alert("You have 10 guesses to guess the Scotch\nGood luck!!")
+//     randomScotch();
+//     calculateNumberOfLetters();
+// }
+
+
+//Added event listener CHECKBACK
+// onkeydown (){
+// guessCount++;
+// }
+ 
+// console.log(guessCount)
+
+//  WAS:
+// document.addEventListener("keypress", function (pressed) {
+//     keyPressed = pressed.key.toUpperCase();
+
+// });
+// Console.log(pressed.key)
+
+
+// UNTESTED reset game
+// function reset() {
+//     guessesAllowed = 10;
+//     guessCount = [];
+//     scotch; //does this reset??
+//     guessTheScotch; //does this reset??
+// }
+
+
+
+// WIN OR LOSE
+// function winOrLose() {
+//     if (guessCount === guessesAllowed);
+//     reset(); 
+//     alert("Maybe you\'ve had enough Scotch already...\n\n Play again?");
+//     // else if **TBD**; 
+// }
+
+
+
+// charat not working
+
+// Attempt at locating key pressed in the hidden word
+    // for (var i = 0; i < scotch.length; i++) {
+    //     if (scotch.charAt[i] === keyPressed) {
+    //         if (i !== -1) {
+    //             guessTheScotch[i] = keyPressed;
+    //             document.getElementById("guessTheScotch").innerHTML = guessTheScotch;
+    //         }
+    //         else {guesses--}
+    //     }
+
+    // }
+
+
+// // guesses loop key listener
+// document.onkeyup() {
+//     f{
+//         if (g < 11) {
+//             alert("You have " + (10 -= g) + " guesses left.")
+//         }
+//         else {
+//             alert("Maybe you've had a little too much Scotch already...\nDo you want to try again?");
+//             resetGame();
+//         }
+//     }
+
+
+
+//     // Array of Scotches (20 varieties)
+//     var scotches = ["Ardbeg", "Bowmore", "Cragganmore", "Dalwhinnie", "Edradour", "Finlaggan", "Glenlivet", "Hillside", "Inchmoan", "Jura", "Kininvie", "Lagavulin", "Macduff", "Oban", "Pittyvaich", "Rosebank", "Smokehead", "Talisker", "Various", "Wolfburn"],
+
+
+
+//         // random select a Scotch from Array 
+//         function randomScotch(i) {
+//             scotches[i] = scotch;
+//             return scotch;
+//             // console.log(scotch)
+//         }
+
+//     // create an array of letter in the word or other way to seperate the characters
+
+
+//     // Display Letters Hidden but with underscore 
+
+
+
+
+
+//     // If all letters guessed than show
+
+//     // If all letters guessed that alert("YOU WIN! \nEnjoy yourself a nice glass of Scotch!")
+
+
+//     // You Win Else You Lose
+
+//     //  reset the game 
+
+//     //  setting up start of game 
+
+//     resetGame(){
+//         alert("You have 10 guesses to guess the Scotch\nGood luck!!")
+//         randomScotch();
+//         calculateNumberOfLetters();
+//     }
+
+
+//     // guesses loop key listener
+
+//     for (g = 1; g <= 10; i++) {
+//         // key listener that counts and adds to guesses g
+//         if (g === 10);
+//         alert("Maybe you've had a little too much Scotch already");
+//         resetGame();
+//     }
+
+//     // Array of Scotches (20 varieties)
+
+
+
+
+//         // random select a Scotch from Array 
+//         randomScotch(i) {
+//             Math.floor(Math.random() * scotches.length) = [i];
+//             scotches[i] = scotch;
+//             return scotch;
+//             // console.log(scotch)
+//         }
+
+// // create an array of letter in the word or other way to seperate the characters
+
+
+// // Display Letters Hidden but with underscore 
+
+
+
+
+
+// // If all letters guessed than show
+
+// // If all letters guessed that alert("YOU WIN! \nEnjoy yourself a nice glass of Scotch!")
+
+
+// // You Win Else You Lose
+
+// //  reset the game 
